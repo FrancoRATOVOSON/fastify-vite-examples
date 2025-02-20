e613efcf1d62f681a0b0aaa4a81415fb70a37888
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-02-20 20:06:44 +0300

    Update vite.config.ts

--- fastify-vite/react-vanilla-spa/vite.config.ts
+++ fastify-vite/react-vanilla-spa/vite.config.ts
@@ -11,4 +11,8 @@ import { viteFastify } from '@fastify/vite/plugin'
 export default defineConfig(() => ({
   root: join(import.meta.dirname, 'client'),
   plugins: [tailwindcss(), viteFastify(), viteReact(), tsconfigPaths()],
+  build: {
+    emptyOutDir: true,
+    outDir: join(import.meta.dirname, 'dist/client'),
+  },
 }));
